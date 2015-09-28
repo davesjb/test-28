8a3ce03e106ffdef368e61466b872827f8ec24f6
davesjb <davesjb@gmail.com>
2015-09-28 20:03:04 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>
+<html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>
+<html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>
+<html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang="en">
+<!--<![endif]-->
+<!-- Written by Dave Beckett-->
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=yes">
+    <meta name="HandheldFriendly" content="true">
+    <meta name="MobileOptimized" content="320">
+    <title>Brand Keywords</title>
+
+    <link href="css/elements.css" rel="stylesheet" type="text/css"/>
+    <link href="css/global.css" rel="stylesheet" type="text/css"/>
+    <link href="css/brand.css" rel="stylesheet" type="text/css"/>
+    <link href="css/jquery.mmenu.css" rel="stylesheet" type="text/css"/>
+    <link href="css/jquery.mmenu.positioning.css" rel="stylesheet" type="text/css"/>
+
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+    <script type="text/javascript" src="js/jquery-1.8.3.min.js"></script>
+    <script type="text/javascript" src="js/jquery.mmenu.min.all.js"></script>
+    
+    <script type="text/javascript">
+    $(document).ready(function() {
+        $("nav#menu").mmenu({
+            offCanvas: {
+               position  : "right"
+            }
+         });
+    });
+    </script>
+
+    <meta name="keywords" content="Lorem, Ipsum"/>
+    <meta name="description" content="Lorem ipsum"/>
+</head>
+<body id="top">
+
+    <div id="brand" class="container-fluid">
+
+        <header class="header container">
+            <img src="images/brand-logo.png" alt="logo" border="0" class="logo" />
+            <a href="#menu" id="burger"></a>
+        </header>
+
+        <div class="nav-outer container-fluid">
+            <nav class="global-nav container">
+                <ul>
+                    <li><a href="#" class="active">Home</a></li>
+                    <li><a href="#">About us</a></li>
+                    <li><a href="#">Graduate Programmes</a></li>
+                    <li><a href="#">Interships</a></li>
+                    <li><a href="#">Search Jobs and Apply</a></li>
+                </ul>   
+            </nav>
+            <nav id="menu">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About us</a></li>
+                    <li><a href="#">Graduate Programmes</a></li>
+                    <li><a href="#">Interships</a></li>
+                    <li><a href="#">Search Jobs and Apply</a></li>
+                </ul>   
+            </nav>
+        </div>
+
+        <div class="main-banner container-fluid">
+            <h1>We <span>love</span><br>the positively<br>perplexing</h1>
+        </div>
+
+        <div class="search-outer container-fluid">
+            <div class="search-bar container">
+                <div class="fieldset">
+                    <input class="search-input" type="text" placeholder="Search Jobs and apply" name="keywords-visible">
+                    <button class="search-button" type="submit"><img src="images/search.png" border="0" alt="search"></button>
+                </div>
+            </div>
+        </div>
+
+        <div class="content-outer container-fluid">
+            <section class="homepage-content container">
+                <h2>Graduate careers &amp; internships</h2>
+                <p>Welcome to Company. We’re a leading engineering and technology company where solving mind-bending problems is our job, 
+                    our passion – our world. If you’re a graduate keen to embark on a stimulating engineering, business or finance career, 
+                    or an undergraduate looking for an insightful internship or placement, it could be your world too. Read about our graduate 
+                    and internship programmes in the UK. Find out how you can develop. Discover what we’re doing in Security, Defence, Aerospace 
+                    and Transport. Delve into this site – and <a href="#">watch the video</a>.
+                </p>
+            </section>
+        </div>
+
+        <div class="categories-outer">
+            <div class="categories row">
+                <div class="one-third roles"><a href="#">Roles</a></div>
+                <div class="one-third open-events"><a href="#">Open events</a></div>
+                <div class="one-third recruitment-process"><a href="#"><span>Recruitment process</span></a></div>
+            </div>
+        </div>
+
+        <footer class="container">
+           <div class="copyright">&copy; Copyright Company 2014</div>
+           <img src="images/icons.png" alt="icons" class="icons" />
+        </footer>
+    </div>
+    <!-- Go to www.addthis.com/dashboard to customize your tools -->
+    <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-560949c9c578f41f" async="async"></script>
+    <!-- Go to www.addthis.com/dashboard to customize your tools -->
+    <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-560949c9c578f41f" async="async"></script>
+
+</body>
+</html>
